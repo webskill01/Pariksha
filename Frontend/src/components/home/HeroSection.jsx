0e0e4492f8d19acaec9b5504a0664f1ce52399c7
Nitin <webskillset@gmail.com>
2025-08-15 15:00:08 +0530

    Update HeroSection.jsx
    Edited Alert

--- Frontend/src/components/home/HeroSection.jsx
+++ Frontend/src/components/home/HeroSection.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import DismissibleAlert from '../../utils/ALert';
+import DismissibleAlert from '../../utils/Alert';
 import { 
   Search, 
   LibraryBooks,
